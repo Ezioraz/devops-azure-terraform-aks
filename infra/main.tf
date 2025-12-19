d44ac35836145d98557675f48841d1708b78062f
Javid Akthar <ezioraz@gmail.com>
2025-12-19 19:17:37 +0530

    Clean repo: remove terraform state and secrets

---
+++ infra/main.tf
@@ -0,0 +1,39 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "devops-aks-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "devopsaks"
+
+  default_node_pool {
+    name       = "system"
+    node_count = 1
+    vm_size    = "Standard_DC2s_v3"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  default = "eastus"
+}
+
+variable "resource_group_name" {
+  default = "devops-aks-rg"
+}
+
+variable "acr_name" {
+  default = "flaskaksacr1234"
+}
